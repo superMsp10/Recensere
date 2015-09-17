501ec58d8185caf85ad139a30d870a4bb32291e3
superMsp10 <pandeymahan34@gmail.com>
2015-09-16 22:52:45 -0400

    player network init
    made function that will only be called by the local player, put camera
    and other player mechanics init here

---
+++ unity_project/Assets/Scripts/player.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class player : MonoBehaviour
+{
+		public Camera thisCam;
+		// Use this for initialization
+		void Start ()
+		{
+		}
+
+		public void networkInit ()
+		{
+				Debug.Log ("hello");
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+	
+		}
+}
