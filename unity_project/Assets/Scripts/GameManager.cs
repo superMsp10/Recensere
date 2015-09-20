3681b00fa2120b9a7b4098fa9a1c29f6c1625bb8
superMsp10 <pandeymahan34@gmail.com>
2015-09-19 20:21:35 -0400

    maze example

---
+++ unity_project/Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour
+{
+
+		public Maze mazePrefab;
+
+		private Maze mazeInstance;
+
+		private void Start ()
+		{
+				BeginGame ();
+		}
+	
+		private void Update ()
+		{
+				if (Input.GetKeyDown (KeyCode.Space)) {
+						RestartGame ();
+				}
+		}
+
+		private void BeginGame ()
+		{
+				mazeInstance = Instantiate (mazePrefab) as Maze;
+				mazeInstance.Generate ();
+		}
+
+		private void RestartGame ()
+		{
+				StopAllCoroutines ();
+				Destroy (mazeInstance.gameObject);
+				BeginGame ();
+		}
+}
