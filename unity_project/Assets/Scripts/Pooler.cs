f4d883970ee35035de1cca638e52bb8276ca22f1
superMsp10 <pandeymahan34@gmail.com>
2015-10-11 22:09:27 -0400

    Pooler class and Poolable interface

---
+++ unity_project/Assets/Scripts/Pooler.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Pooler : MonoBehaviour
+{
+		public List<Poolable> active;
+		public List<Poolable> useable;
+
+
+		public Pooler (int min, int max)
+		{
+		}
+
+		public void getObject ()
+		{
+
+		}
+
+		public void disposeObject ()
+		{
+		
+		}
+}
