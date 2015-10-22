508984dd6ad10d7546e45dd9b2bcff9fb285632b
superMsp10 <pandeymahan34@gmail.com>
2015-10-21 20:08:35 -0400

    usrname and password validation

--- unity_project/Assets/Scripts/StartingUI.cs
+++ unity_project/Assets/Scripts/StartingUI.cs
@@ -21,7 +21,55 @@ public class StartingUI : MonoBehaviour,UIState
 				gameObject.SetActive (false);
 
 		}
-	
+
+		public void CreateAccount ()
+		{
+				if (validateUsername (userName.text) && validatePassword (password.text)) {
+						Debug.Log ("hello");
+				}
+		
+
+		}
+
+		bool validateUsername (string s)
+		{
+
+				if (s.Length < 3) {
+						error.text = "Username has to be longer than 3 characters, please choose a longer username to continue";
+						return false;
+				} 
+				if (s.Length > 15) {
+						error.text = "Username has to be shorter than 15 characters, please choose a shorter username to continue";
+						return false;
+				} 
+				if (s.Contains (" ")) {
+						error.text = "Username cannot include spaces, please remove all spaces to continue";
+						return false;
+				}
+				error.text = "";
+				return true;
+
+		}
+		bool validatePassword (string s)
+		{
+		
+				if (s.Length < 5) {
+						error.text = "Password has to be longer than 5 characters, please choose a longer password to continue";
+						return false;
+				}
+				if (s.Length > 15) {
+						error.text = "Password has to be shorter than 15 characters, please choose a shorter password to continue";
+						return false;
+				} 
+				if (s.Contains (" ")) {
+						error.text = "Password cannot include spaces, please remove all spaces to continue";
+						return false;
+				}
+				error.text = "";
+				return true;
+		
+		}
+
 		public	void UpdateUI ()
 		{
 		
