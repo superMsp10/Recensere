4842af7b6bd764a23c052a6730f0af06424c868c
superMsp10 <pandeymahan34@gmail.com>
2016-03-02 11:54:18 -0500

    tap then hold to use jetpack
    disables gravity

--- unity_project/Assets/Scripts/playerMove.cs
+++ unity_project/Assets/Scripts/playerMove.cs
@@ -3,27 +3,25 @@ using System.Collections;
 
 public class playerMove : MonoBehaviour
 {
-
+		public float og_speed;		
 		public float speed;		
 		public float fly_speed;		
-
-		public float og_speed;		
-
 		public float speedLimitMultiplier;
-
-
+	
 		public float jumpPower;
-
-		Rigidbody rigidbod;
-
 		public bool jumped = false;
 		public bool grounded = false;
+		public double fuel;
 
 		public	Transform start;
 		public	Transform feets;
 		public LayerMask whatGround;
 
 		public Animator anim;
+		Rigidbody rigidbod;
+	
+	
+
 
 		void Start ()
 		{
@@ -39,12 +37,17 @@ public class playerMove : MonoBehaviour
 		void checkJump ()
 		{
 
-				grounded = Physics.Linecast (start.position, feets.position, whatGround);
+				grounded = (Physics.Linecast (start.position, feets.position, whatGround));
 
-				if (grounded && Input.GetAxis ("Jump") > 0) {
-						if (!jumped)
+				if (Input.GetAxis ("Jump") > 0) {
+						if (grounded && !jumped) {
 								jump ();
+						} else if (fuel > 0) {
+								jetPack ();
+						}
 
+				} else {
+						rigidbod.useGravity = true;
 				}
 		}
 
@@ -53,6 +56,11 @@ public class playerMove : MonoBehaviour
 				rigidbod.velocity = new Vector3 (rigidbod.velocity.x * fly_speed, jumpPower, rigidbod.velocity.z * fly_speed);
 
 				jumped = true;
+		}
+
+		void jetPack ()
+		{
+				rigidbod.useGravity = false;
 		}
 
 		void checkMovement ()
