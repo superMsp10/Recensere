7dad3aded5b363edc65cfcd145976236732d4367
superMsp10 <pandeymahan34@gmail.com>
2016-03-12 10:47:55 -0500

    jetpack afterburner and air intake particles

--- unity_project/Assets/Scripts/JetpackFuelDisplay.cs
+++ unity_project/Assets/Scripts/JetpackFuelDisplay.cs
@@ -8,7 +8,7 @@ public class JetpackFuelDisplay : MonoBehaviour
 	
 		public void setFuel (float fuelOutOfMax)
 		{
-				transform.localScale = new Vector3 (maxDisplay, Mathf.Lerp (0, maxDisplay, fuelOutOfMax), maxDisplay);
+				transform.localScale = new Vector3 (transform.localScale.x, Mathf.Lerp (0, maxDisplay, fuelOutOfMax), transform.localScale.z);
 				transform.localPosition = new Vector3 (0, 0, transform.localScale.y / 2);
 		}
 }
