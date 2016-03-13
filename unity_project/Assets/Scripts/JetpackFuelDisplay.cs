7037fe2037faaf3f09eaf28faf52995bd697df03
superMsp10 <pandeymahan34@gmail.com>
2016-03-12 20:08:20 -0500

    Jetpack fuel display fix
    keeps local position for x and y

--- unity_project/Assets/Scripts/JetpackFuelDisplay.cs
+++ unity_project/Assets/Scripts/JetpackFuelDisplay.cs
@@ -9,6 +9,6 @@ public class JetpackFuelDisplay : MonoBehaviour
 		public void setFuel (float fuelOutOfMax)
 		{
 				transform.localScale = new Vector3 (transform.localScale.x, Mathf.Lerp (0, maxDisplay, fuelOutOfMax), transform.localScale.z);
-				transform.localPosition = new Vector3 (0, 0, transform.localScale.y / 2);
+				transform.localPosition = new Vector3 (transform.localPosition.x, transform.localPosition.y, transform.localScale.y / 2);
 		}
 }
