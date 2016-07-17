23cc36f79d275724196a4b4d2bdee0d7a86db7f1
superMsp10 <pandeymahan34@gmail.com>
2016-07-17 12:44:09 -0400

    item pickup
    fixed: player picks up items without inventory space

--- unity_project/Assets/Items/TesCube/item_Cube.cs
+++ unity_project/Assets/Items/TesCube/item_Cube.cs
@@ -1,243 +1,263 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class item_Cube : MonoBehaviour,Holdable
+public class item_Cube : MonoBehaviour, Holdable
 {
 
-		//Holdable Stuff
-		public Sprite _holdUI;
-		public bool _pickable;
-		public int _amount;
-			
-		//Pooler
-		[HideInInspector]
-		public NetworkPooler
-				projectilePooler = null;
-		public float itemReset;
-		public int maxItems;
-		public GameObject projectile;
-		public float throwMultiplier;
-		public string projectileLayer;
-		public string itemLayer;
+    //Holdable Stuff
+    public Sprite _holdUI;
+    public bool _pickable;
+    public int _amount;
 
-		//Item Stuff
-		public Rigidbody r;
-		player p;
-		public Color normal;
-		public Color highlighted;
-		float timeStarted;
-		float timeEnded;
-		public float wantedTime;
-		public PhotonView thisView;
-		int playerID;
-		bool selected = false;
-		bool startedHold = false;
-		Renderer ren;
+    //Pooler
+    [HideInInspector]
+    public NetworkPooler
+            projectilePooler = null;
+    public float itemReset;
+    public int maxItems;
+    public GameObject projectile;
+    public float throwMultiplier;
+    public string projectileLayer;
+    public string itemLayer;
 
-		void Start ()
-		{
-				projectilePooler = new NetworkPooler (maxItems, projectile);
-				ren = GetComponent<Renderer> ();
+    //Item Stuff
+    public Rigidbody r;
+    player p;
+    public Color normal;
+    public Color highlighted;
+    float timeStarted;
+    float timeEnded;
+    public float wantedTime;
+    public PhotonView thisView;
+    int playerID;
+    bool selected = false;
+    bool startedHold = false;
+    Renderer ren;
 
-		}
+    void Start()
+    {
+        projectilePooler = new NetworkPooler(maxItems, projectile);
+        ren = GetComponent<Renderer>();
 
-		//Item------------------------------------------//
-		public	void Update ()
-		{
-				if (selected) {
-						if (ren == null) {
-								Debug.Log ("No renderer on this object");
-								return;
-						}
-						if (startedHold) {
-								ren.material.color = Color.Lerp (normal, highlighted, (Time.time - timeStarted) / wantedTime);
-						} else {
-								ren.material.color = normal;
-						}
-				}
-		}
+    }
 
-		public	void detach (GameObject g)
-		{
-				projectilePooler.disposeObject (g.GetComponent<Poolable> ());
-		
-		}
+    //Item------------------------------------------//
+    public void Update()
+    {
+        if (selected)
+        {
+            if (ren == null)
+            {
+                Debug.Log("No renderer on this object");
+                return;
+            }
+            if (startedHold)
+            {
+                ren.material.color = Color.Lerp(normal, highlighted, (Time.time - timeStarted) / wantedTime);
+            }
+            else
+            {
+                ren.material.color = normal;
+            }
+        }
+    }
 
-		void OnCollisionEnter (Collision collision)
-		{
-				if (pickable) {
-						if (collision.collider.gameObject.layer == LayerMask.NameToLayer (GameManager.thisM.PlayerLayer)) {
-								Debug.Log ("OnCollisionEnter, Cube and Player Amount: " + _amount);
-//								p = collision.collider.gameObject.GetComponent<player> ();
-								playerID = collision.collider.gameObject.GetComponent<PhotonView> ().viewID;
-								thisView.RPC ("pickedUpBy", PhotonTargets.All, playerID);
+    public void detach(GameObject g)
+    {
+        projectilePooler.disposeObject(g.GetComponent<Poolable>());
 
-								invManager.thisInv.addHoldable (this, _amount);
-								_pickable = false;
-						}
-				}
-		}
-		
-		//Holdable------------------------------------------//
-		public Sprite holdUI {
-				get {
-						return _holdUI; 
-				}
-		
-		}
-		public int stackSize {
-				get {
-						return 16; 
-				}
-		
-		}
-		public string description {
-				get {
-						return "<b>Hello! <color=red>My name is, </color> Cube, </b>I will be <i><color=blue>helping you</color> test!</i>"; 
-				}
-		
-		}
-		public bool pickable {
-				get {
-						return _pickable; 
-				}
-		
-		}
-		public int amount {
-				get {
-						return _amount; 
-				}
-				set {
-						_amount = value; 
-				}
-		}
-		public	bool  buttonDown ()
-		{
-				thisView.RPC ("buttonDownBy", PhotonTargets.All, null);
-	
-				return false;
-		}
+    }
 
-		[PunRPC]
-		void  buttonDownBy ()
-		{
-				//				Debug.Log ("buttonDown by Cube");
-				timeStarted = Time.time;
-				startedHold = true;
-		}
+    void OnCollisionEnter(Collision collision)
+    {
+        if (pickable)
+        {
+            if (collision.collider.gameObject.layer == LayerMask.NameToLayer(GameManager.thisM.PlayerLayer))
+            {
+                Debug.Log("OnCollisionEnter, Cube and Player Amount: " + _amount);
+                //								p = collision.collider.gameObject.GetComponent<player> ();
+                if (invManager.thisInv.addHoldable(this, _amount) <= 0)
+                {
+                    playerID = collision.collider.gameObject.GetComponent<PhotonView>().viewID;
+                    thisView.RPC("pickedUpBy", PhotonTargets.All, playerID);
 
-		public	void  buttonUP ()
-		{
-				thisView.RPC ("buttonUpBy", PhotonTargets.All, null);
+                    _pickable = false;
+                }
+            }
+        }
+    }
 
-				//Projectile Stuff
-				GameObject g = projectilePooler.getObject ();
-				//Set Transform to this and reset Timer
-				g.transform.position = transform.position;
-				g.transform.rotation = transform.rotation;
-				g.GetComponent<Timer> ().StartTimer (itemReset);
-				cube_Projectile c = g.GetComponent<cube_Projectile> ();
-				c.thisPooler = this;
-				c.armed = true;
+    //Holdable------------------------------------------//
+    public Sprite holdUI
+    {
+        get
+        {
+            return _holdUI;
+        }
 
-				//Apply Force
-				float force = 0f;
-				float heldTime = Time.time - timeStarted;
-				if (heldTime > wantedTime)
-						force = throwMultiplier;
-				else
-						force = (heldTime / wantedTime) * throwMultiplier;
-				g.GetComponent<Rigidbody> ().AddForce (p.left_hand.forward * force);
-		}
+    }
+    public int stackSize
+    {
+        get
+        {
+            return 16;
+        }
 
-		[PunRPC]
-		void  buttonUpBy ()
-		{
-				timeEnded = Time.time;
-				startedHold = false;
-				//				Debug.Log ("buttonUP by Cube");
-		}
+    }
+    public string description
+    {
+        get
+        {
+            return "<b>Hello! <color=red>My name is, </color> Cube, </b>I will be <i><color=blue>helping you</color> test!</i>";
+        }
 
-		public	void  onSelect ()
-		{
-				thisView.RPC ("selectedBy", PhotonTargets.All, null);
-		}
+    }
+    public bool pickable
+    {
+        get
+        {
+            return _pickable;
+        }
 
-		[PunRPC]
-		void selectedBy ()
-		{
-				gameObject.SetActive (true);
-				selected = true;
-		}
+    }
+    public int amount
+    {
+        get
+        {
+            return _amount;
+        }
+        set
+        {
+            _amount = value;
+        }
+    }
+    public bool buttonDown()
+    {
+        thisView.RPC("buttonDownBy", PhotonTargets.All, null);
 
-		public void  onDeselect ()
-		{
-				thisView.RPC ("deselectedBy", PhotonTargets.All, null);
-		}
+        return false;
+    }
 
-		[PunRPC]
-		void deselectedBy ()
-		{
-				//				Debug.Log ("onDeselect by Cube");
-				gameObject.SetActive (false);
-				selected = false;
+    [PunRPC]
+    void buttonDownBy()
+    {
+        //				Debug.Log ("buttonDown by Cube");
+        timeStarted = Time.time;
+        startedHold = true;
+    }
 
-		}
-		public	void  onPickup ()
-		{
-//				Debug.Log ("onPickup by Cube");
-				thisView.TransferOwnership (PhotonNetwork.player.ID);
-		}
+    public void buttonUP()
+    {
+        thisView.RPC("buttonUpBy", PhotonTargets.All, null);
 
-		[PunRPC]
-		void pickedUpBy (int viewID)
-		{
-				GetComponent<Renderer> ().material.color = normal;
-				p = GameManager.thisM.getPlayerByViewID (viewID);
-				_pickable = false;
-				r.isKinematic = true;
-				gameObject.layer = LayerMask.NameToLayer (p.handLayer);
-				transform.parent = p.right_hand;
-				transform.position = p.right_hand.position;
-				transform.rotation = p.right_hand.rotation;
-				gameObject.SetActive (false);
-		}
+        //Projectile Stuff
+        GameObject g = projectilePooler.getObject();
+        //Set Transform to this and reset Timer
+        g.transform.position = transform.position;
+        g.transform.rotation = transform.rotation;
+        g.GetComponent<Timer>().StartTimer(itemReset);
+        cube_Projectile c = g.GetComponent<cube_Projectile>();
+        c.thisPooler = this;
+        c.armed = true;
 
-		public	void  onDrop ()
-		{
-				thisView.RPC ("droppedBy", PhotonTargets.All, null);
+        //Apply Force
+        float force = 0f;
+        float heldTime = Time.time - timeStarted;
+        if (heldTime > wantedTime)
+            force = throwMultiplier;
+        else
+            force = (heldTime / wantedTime) * throwMultiplier;
+        g.GetComponent<Rigidbody>().AddForce(p.left_hand.forward * force);
+    }
 
-		}
+    [PunRPC]
+    void buttonUpBy()
+    {
+        timeEnded = Time.time;
+        startedHold = false;
+        //				Debug.Log ("buttonUP by Cube");
+    }
 
-		[PunRPC]
-		void  droppedBy ()
-		{
-				//				Debug.Log ("onDrop by Cube");
-				GetComponent<Renderer> ().material.color = normal;
-				r.velocity = Vector3.zero;
-				r.isKinematic = false;
-				transform.parent = GameManager.thisM.currLevel.items;
-				gameObject.layer = LayerMask.NameToLayer (itemLayer);
+    public void onSelect()
+    {
+        thisView.RPC("selectedBy", PhotonTargets.All, null);
+    }
 
-				gameObject.SetActive (true);
-				//				transform.position = p.transform.position;
-				Invoke ("resetPick", 5.0f);
-		}
+    [PunRPC]
+    void selectedBy()
+    {
+        gameObject.SetActive(true);
+        selected = true;
+    }
 
-		[PunRPC]
-		void resetPickBy ()
-		{
-//				Debug.Log ("ResetPick by Cube");
-				_pickable = true;
+    public void onDeselect()
+    {
+        thisView.RPC("deselectedBy", PhotonTargets.All, null);
+    }
 
-		}
+    [PunRPC]
+    void deselectedBy()
+    {
+        //				Debug.Log ("onDeselect by Cube");
+        gameObject.SetActive(false);
+        selected = false;
 
-		public	void resetPick ()
-		{
-				thisView.RPC ("resetPickBy", PhotonTargets.All, null);
-		}
-		
+    }
+    public void onPickup()
+    {
+        //				Debug.Log ("onPickup by Cube");
+        thisView.TransferOwnership(PhotonNetwork.player.ID);
+    }
 
-		
+    [PunRPC]
+    void pickedUpBy(int viewID)
+    {
+        GetComponent<Renderer>().material.color = normal;
+        p = GameManager.thisM.getPlayerByViewID(viewID);
+        _pickable = false;
+        r.isKinematic = true;
+        gameObject.layer = LayerMask.NameToLayer(p.handLayer);
+        transform.parent = p.right_hand;
+        transform.position = p.right_hand.position;
+        transform.rotation = p.right_hand.rotation;
+        gameObject.SetActive(false);
+    }
+
+    public void onDrop()
+    {
+        thisView.RPC("droppedBy", PhotonTargets.All, null);
+
+    }
+
+    [PunRPC]
+    void droppedBy()
+    {
+        //				Debug.Log ("onDrop by Cube");
+        GetComponent<Renderer>().material.color = normal;
+        r.velocity = Vector3.zero;
+        r.isKinematic = false;
+        transform.parent = GameManager.thisM.currLevel.items;
+        gameObject.layer = LayerMask.NameToLayer(itemLayer);
+
+        gameObject.SetActive(true);
+        //				transform.position = p.transform.position;
+        Invoke("resetPick", 5.0f);
+    }
+
+    [PunRPC]
+    void resetPickBy()
+    {
+        //				Debug.Log ("ResetPick by Cube");
+        _pickable = true;
+
+    }
+
+    public void resetPick()
+    {
+        thisView.RPC("resetPickBy", PhotonTargets.All, null);
+    }
+
+
+
 
 }
