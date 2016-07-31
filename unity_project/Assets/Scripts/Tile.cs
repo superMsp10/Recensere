32207ce248118098da1e0ff76e23c16e319c7321
superMsp10 <pandeymahan34@gmail.com>
2016-07-31 12:34:15 -0400

    player cannot destroy tiles in level 1

--- unity_project/Assets/Scripts/Tile.cs
+++ unity_project/Assets/Scripts/Tile.cs
@@ -1,160 +1,182 @@
 using UnityEngine;
 using System.Collections.Generic;
 
-public abstract class Tile: MonoBehaviour, Health, Attachable
+public abstract class Tile : MonoBehaviour, Health, Attachable
 {
-		//Health
-		public	float health;
-		public	string lastAttacker;
-		public	bool takeDmg = true;
-		public float Sturdy = 10f;
-		float orgHealth;
-		public LayerMask damagedBy;
+    //Health
+    public float health;
+    public string lastAttacker;
+    public bool takeDmg = true;
+    public float Sturdy = 10f;
+    float orgHealth;
+    public LayerMask damagedBy;
 
-		//Health Representation
-		Renderer thisRender;
-		public Color damaged;
+    //Health Representation
+    Renderer thisRender;
+    public Color damaged;
 
-		//Tile Transform
-		public int xPos;
-		public int yPos;
-		public bool yWall = false;
+    //Tile Transform
+    public int xPos;
+    public int yPos;
+    public bool yWall = false;
 
-		//Decal
-		public int decalLimit;
-		List<Poolable> Attached;
-
-
-		public int tileSize = 4;
+    //Decal
+    public int decalLimit;
+    List<Poolable> Attached;
 
 
-		void Start ()
-		{
-				Attached = new List<Poolable> ();
-				orgHealth = health;
-				thisRender = GetComponent<Renderer> ();
-				if (thisRender == null)
-						Debug.Log ("No renderer found on this object, damage color representation will not suceed");
-		}
-
-		public	virtual bool takeDamage (float damage, string attacker)
-		{
-//				Debug.Log ("Take Damage From Tile");
-
-				if (health <= 0)
-						return false;
-				HP -= damage;
-				lastAttacker = attacker;
-				if (health <= 0) {
-						Destroy ();
-						return true;
-				}
-				return false;
-
-		}
-
-		public	virtual void syncDamage (float damage, string attacker)
-		{
-//				Debug.Log ("Sync Damage From Tile");
-
-				HP -= damage;
-				lastAttacker = attacker;
-				if (health <= 0) {
-						Destroy ();
-				}
-		
-		}
+    public int tileSize = 4;
 
 
-		public virtual	void Destroy ()
-		{
-				if (Attached != null) {
-						for (int i = 0; Attached.Count>0; i++) {
-								detach (Attached [0].gameobject);
+    void Start()
+    {
+        Attached = new List<Poolable>();
+        orgHealth = health;
+        thisRender = GetComponent<Renderer>();
+        if (thisRender == null)
+            Debug.Log("No renderer found on this object, damage color representation will not suceed");
+    }
+
+    public virtual bool takeDamage(float damage, string attacker)
+    {
+        //				Debug.Log ("Take Damage From Tile");
+
+        if (health <= 0)
+            return false;
+        HP -= damage;
+        lastAttacker = attacker;
+        if (health <= 0)
+        {
+            Destroy();
+            return true;
+        }
+        return false;
+
+    }
+
+    public virtual void syncDamage(float damage, string attacker)
+    {
+        //				Debug.Log ("Sync Damage From Tile");
+
+        HP -= damage;
+        lastAttacker = attacker;
+        if (health <= 0)
+        {
+            Destroy();
+        }
+
+    }
 
 
-						}
-				}
-				Destroy (gameObject);
-			
-		}
-		public	 string lastDamageBy ()
-		{
-				return lastAttacker;
-		}
+    public virtual void Destroy()
+    {
+        if (Attached != null)
+        {
+            for (int i = 0; Attached.Count > 0; i++)
+            {
+                detach(Attached[0].gameobject);
 
-		public float HP {
-				get {
-						return health; 
-				}
-				set {
-						health = value; 
-						thisRender.material.color = Color.Lerp (damaged, Color.white, health / orgHealth);
 
-				}
-		}
+            }
+        }
+        Destroy(gameObject);
 
-		public float Sturdyness {
-				get {
-						return Sturdy; 
-				}
-	
-		}
+    }
+    public string lastDamageBy()
+    {
+        return lastAttacker;
+    }
 
-		public int limit {
-				get {
-						return decalLimit; 
-				}
-		
-		}
+    public float HP
+    {
+        get
+        {
+            return health;
+        }
+        set
+        {
+            health = value;
+            thisRender.material.color = Color.Lerp(damaged, Color.white, health / orgHealth);
 
-		public List<Poolable> attached {
-				get {
-						return Attached; 
-				}
-		
-		}
+        }
+    }
 
-		public	 void attach (GameObject g)
-		{
+    public float Sturdyness
+    {
+        get
+        {
+            return Sturdy;
+        }
 
-				if (g != null) {
-						Attached.Add (g.GetComponent<Poolable> ());
-				} else
-						Debug.Log ("Game object you are trying to attach is null");
-		}
+    }
 
-		public	 void detach (GameObject g)
-		{
-				Attached.Remove (g.GetComponent<Poolable> ());
-		}
+    public int limit
+    {
+        get
+        {
+            return decalLimit;
+        }
 
-		void OnCollisionEnter (Collision collision)
-		{
+    }
 
-				if (damagedBy == (damagedBy | (1 << collision.gameObject.layer))) {
-						float sdm = GameManager.speedToDamageMultiplier;
-//				Debug.Log ("Collision Enter at Tile");
-						float dmg = Mathf.Pow (collision.relativeVelocity.magnitude, sdm);
-						if (takeDmg && collision.relativeVelocity.magnitude > Sturdy) {
-						
-								if (takeDamage (dmg, collision.collider.name)) {
-										if (collision.collider.attachedRigidbody != null) {
-												collision.collider.attachedRigidbody.velocity *= sdm;
-										}
-								} else {
-										addFX (collision, dmg);
-								}
+    public List<Poolable> attached
+    {
+        get
+        {
+            return Attached;
+        }
 
-		
-						}
-				}
-		}
+    }
 
-		public virtual void addFX (Collision c, float dmg)
-		{
-				Debug.Log ("Default FX Call");
-		}
+    public void attach(GameObject g)
+    {
 
-	
+        if (g != null)
+        {
+            Attached.Add(g.GetComponent<Poolable>());
+        }
+        else
+            Debug.Log("Game object you are trying to attach is null");
+    }
+
+    public void detach(GameObject g)
+    {
+        Attached.Remove(g.GetComponent<Poolable>());
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Collision Enter at Tile");
+
+        if (damagedBy == (damagedBy | (1 << collision.gameObject.layer)))
+        {
+            Debug.Log("Damage Layer Collision at Player");
+            float sdm = GameManager.speedToDamageMultiplier;
+            float dmg = Mathf.Pow(collision.relativeVelocity.magnitude, sdm);
+            if (takeDmg && collision.relativeVelocity.magnitude > Sturdy)
+            {
+                Debug.Log("Engough Damage at Player");
+                if (takeDamage(dmg, collision.collider.name))
+                {
+                    if (collision.collider.attachedRigidbody != null)
+                    {
+                        collision.collider.attachedRigidbody.velocity *= sdm;
+                    }
+                }
+                else
+                {
+                    addFX(collision, dmg);
+                }
+
+
+            }
+        }
+    }
+
+    public virtual void addFX(Collision c, float dmg)
+    {
+        Debug.Log("Default FX Call");
+    }
+
+
 }
--- unity_project/Assets/Scripts/player.cs
+++ unity_project/Assets/Scripts/player.cs
@@ -2,185 +2,203 @@
 using UnityEngine.UI;
 using System.Collections;
 
-public class player : MonoBehaviour,Health
+public class player : MonoBehaviour, Health
 {
-		GameManager thisM = GameManager.thisM;
-		//Health
-		public	float health;
-		public float originalHealth;
-		public	string lastAttacker;
-		public	bool takeDmg = true;
-		public float Sturdy = 10f;
-		public LayerMask damagedBy;
+    GameManager thisM = GameManager.thisM;
+    //Health
+    public float health;
+    public float originalHealth;
+    public string lastAttacker;
+    public bool takeDmg = true;
+    public float Sturdy = 10f;
+    public LayerMask damagedBy;
 
-		//healthUI
-		private Text HPText;
-	
-		//Network
-		public MonoBehaviour[] networkSet;
-		public int playerID;
-		public PhotonView thisView;
+    //healthUI
+    private Text HPText;
 
-		public GameObject thisCam;
-		public MouseLook look;
-		public MouseLook look2;
+    //Network
+    public MonoBehaviour[] networkSet;
+    public int playerID;
+    public PhotonView thisView;
 
-		//Items
-		public Transform left_hand;
-		public Transform right_hand;
-		public string handLayer;
+    public GameObject thisCam;
+    public MouseLook look;
+    public MouseLook look2;
 
-		//Animation
-		public Animator anim;
-		public Rigidbody r;
-		public GameObject animModel;
+    //Items
+    public Transform left_hand;
+    public Transform right_hand;
+    public string handLayer;
 
-
-		void FixedUpdate ()
-		{
-				anim.SetFloat ("YVelo", r.velocity.y);
-		}
-
-	
-		public	virtual bool takeDamage (float damage, string attacker)
-		{
-
-				if (thisView.isMine) {
-						if (health <= 0)
-								return false;
-						HP -= damage;
-						lastAttacker = attacker;
-						if (health <= 0) {
-								Destroy ();
-								return true;
-						}
-				} else {
-						thisView.RPC ("syncDamage", PhotonTargets.Others, damage, attacker);
-				}
-				return false;
-
-		}
-
-		[PunRPC]
-		public	virtual void syncDamage (float damage, string attacker)
-		{
-
-				if (thisView.isMine) {
-						if (health <= 0)
-								return;
-						HP -= damage;
-						lastAttacker = attacker;
-						if (health <= 0) {
-								Destroy ();
-								return;
-						}
-
-				}
-		}
-	
-	
-		public virtual	void Destroy ()
-		{
-//				Destroy (gameObject);
-				thisM.NetworkDisable ();
-				StartCoroutine (tileDictionary.thisM.pauseUI.GetComponent<pauseUI> ().Respawn (5f));
-		
-		}
-		public	 string lastDamageBy ()
-		{
-				return lastAttacker;
-		}
-	
-		public float HP {
-				get {
-						return health; 
-				}
-				set {
-						health = value;
-						if (thisView.isMine)
-								HPText.text = "Health: " + value;
-				}
-		}
-	
-		public float Sturdyness {
-				get {
-						return Sturdy; 
-				}
-		
-		}
-
-		public void networkInit ()
-		{
-				thisM.ChangeCam (thisCam);
-//				thisCam.gameObject.SetActive (true);
-				foreach (MonoBehaviour m in networkSet) {
-						m.enabled = true;
-				}
-				Rigidbody r = GetComponent<Rigidbody> ();
-				r.velocity = new Vector3 (0, 0, 0);
-				r.useGravity = true;
-
-				playerMove pm = GetComponent<playerMove> ();
-				pm.Start ();
-
-				HPText = tileDictionary.thisM.HPText;
-				HP = originalHealth;
-		}
-
-		public void networkDisable ()
-		{
-//				thisCam.gameObject.SetActive (false);
-				foreach (MonoBehaviour m in networkSet) {
-						m.enabled = false;
-				}
-		
-		}
+    //Animation
+    public Animator anim;
+    public Rigidbody r;
+    public GameObject animModel;
 
 
-		
+    void FixedUpdate()
+    {
+        anim.SetFloat("YVelo", r.velocity.y);
+    }
 
-		public static Color getPlayerColour (int playerID)
-		{
-				int team = playerID % 4;
-				switch (team) {
-				case 0:
-						return Color.blue;
 
-				case 1:
-						return Color.red;
+    public virtual bool takeDamage(float damage, string attacker)
+    {
 
-				case 2:
-						return Color.green;
+        if (thisView.isMine)
+        {
+            if (health <= 0)
+                return false;
+            HP -= damage;
+            lastAttacker = attacker;
+            if (health <= 0)
+            {
+                Destroy();
+                return true;
+            }
+        }
+        else
+        {
+            thisView.RPC("syncDamage", PhotonTargets.Others, damage, attacker);
+        }
+        return false;
 
-				case 3:
-						return Color.magenta;
+    }
 
-			
-				default:
-						break;
-				}
+    [PunRPC]
+    public virtual void syncDamage(float damage, string attacker)
+    {
 
-				return Color.blue;
+        if (thisView.isMine)
+        {
+            if (health <= 0)
+                return;
+            HP -= damage;
+            lastAttacker = attacker;
+            if (health <= 0)
+            {
+                Destroy();
+                return;
+            }
 
-		}
+        }
+    }
 
-		void OnCollisionEnter (Collision collision)
-		{
-				if (damagedBy == (damagedBy | (1 << collision.gameObject.layer))) {
 
-						float sdm = GameManager.speedToDamageMultiplier;
-						//				Debug.Log ("Collision Enter at Tile");
-						float dmg = Mathf.Pow (collision.relativeVelocity.magnitude, sdm);
-						if (takeDmg && collision.relativeVelocity.magnitude > Sturdy) {
-			
-								if (takeDamage (dmg, collision.collider.name)) {
-										if (collision.collider.attachedRigidbody != null) {
-												collision.collider.attachedRigidbody.velocity *= sdm;
-										}
-								} 
-			
-			
-						}
-				}
-		}
+    public virtual void Destroy()
+    {
+        //				Destroy (gameObject);
+        thisM.NetworkDisable();
+        StartCoroutine(tileDictionary.thisM.pauseUI.GetComponent<pauseUI>().Respawn(5f));
+
+    }
+    public string lastDamageBy()
+    {
+        return lastAttacker;
+    }
+
+    public float HP
+    {
+        get
+        {
+            return health;
+        }
+        set
+        {
+            health = value;
+            if (thisView.isMine)
+                HPText.text = "Health: " + value;
+        }
+    }
+
+    public float Sturdyness
+    {
+        get
+        {
+            return Sturdy;
+        }
+
+    }
+
+    public void networkInit()
+    {
+        thisM.ChangeCam(thisCam);
+        //				thisCam.gameObject.SetActive (true);
+        foreach (MonoBehaviour m in networkSet)
+        {
+            m.enabled = true;
+        }
+        Rigidbody r = GetComponent<Rigidbody>();
+        r.velocity = new Vector3(0, 0, 0);
+        r.useGravity = true;
+
+        playerMove pm = GetComponent<playerMove>();
+        pm.Start();
+
+        HPText = tileDictionary.thisM.HPText;
+        HP = originalHealth;
+    }
+
+    public void networkDisable()
+    {
+        //				thisCam.gameObject.SetActive (false);
+        foreach (MonoBehaviour m in networkSet)
+        {
+            m.enabled = false;
+        }
+
+    }
+
+
+
+
+    public static Color getPlayerColour(int playerID)
+    {
+        int team = playerID % 4;
+        switch (team)
+        {
+            case 0:
+                return Color.blue;
+
+            case 1:
+                return Color.red;
+
+            case 2:
+                return Color.green;
+
+            case 3:
+                return Color.magenta;
+
+
+            default:
+                break;
+        }
+
+        return Color.blue;
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        //Debug.Log("Collision Enter at Player");
+        if (damagedBy == (damagedBy | (1 << collision.gameObject.layer)))
+        {
+            //Debug.Log("Damage Layer Collision at Player");
+            float sdm = GameManager.speedToDamageMultiplier;
+            float dmg = Mathf.Pow(collision.relativeVelocity.magnitude, sdm);
+            if (takeDmg && collision.relativeVelocity.magnitude > Sturdy)
+            {
+                //Debug.Log("Engough Damage at Player");
+                if (takeDamage(dmg, collision.collider.name))
+                {
+                    if (collision.collider.attachedRigidbody != null)
+                    {
+                        collision.collider.attachedRigidbody.velocity *= sdm;
+                    }
+                }
+
+
+            }
+        }
+    }
 }
