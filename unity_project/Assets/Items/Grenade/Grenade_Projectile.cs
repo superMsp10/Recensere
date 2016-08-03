523a3d83fd7fad84b220bb6fdab0482e92f20f3f
superMsp10 <pandeymahan34@gmail.com>
2016-08-02 22:53:41 -0400

    explosion particles

--- unity_project/Assets/Items/Grenade/Grenade_Projectile.cs
+++ unity_project/Assets/Items/Grenade/Grenade_Projectile.cs
@@ -7,6 +7,10 @@ class Grenade_Projectile : Item_Throwable_Projectile
     public float explosionDelay = 2f;
     public ParticleSystem explosionFX_Fireball;
     public ParticleSystem explosionFX_Dust;
+    public Renderer thisRenderer;
+    public Collider thisCollider;
+    public Rigidbody thisRigid;
+    public GameObject destroyedGrenade;
 
 
     public override void reset(bool on)
@@ -21,6 +25,11 @@ class Grenade_Projectile : Item_Throwable_Projectile
             Debug.Log("Grenade projectile enabled");
             r.velocity = Vector3.zero;
             Invoke("_explode", explosionDelay);
+
+            thisCollider.enabled = true;
+            thisRenderer.enabled = true;
+            thisRigid.isKinematic = false;
+            destroyedGrenade.SetActive(false);
 
         }
 
@@ -39,6 +48,14 @@ class Grenade_Projectile : Item_Throwable_Projectile
         Debug.Log("Grenade explosion rpc");
         explosionFX_Dust.Play();
         explosionFX_Fireball.Play();
+
+        thisCollider.enabled = false;
+        
+        thisRenderer.enabled = false;
+        destroyedGrenade.SetActive(true);
+        thisRigid.isKinematic = true;
+
+
     }
 
 }
